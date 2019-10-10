c3da666e7398a79aefff221c11caca59e0a314a7
fay128 <82790451@qq.com>
2019-10-10 22:09:45 +0800

    support war

--- src/main/java/training/controller/Application.java
+++ src/main/java/training/controller/Application.java
@@ -3,13 +3,14 @@ package training.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 
 @ComponentScan(basePackages="training.*")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
